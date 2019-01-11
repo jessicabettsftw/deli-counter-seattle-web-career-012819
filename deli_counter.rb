3bc0b53c4cf109f3f8ced625e60205d0652a0b36
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-11 03:47:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,10 @@
 def line(queue)
-  
+  if queue.size == 0
+    puts "The line is currently empty."
+  else #there are people in the line
+    print "The line is currently: "
+    position = 1
+    queue.each do |element|
+      print "#{position}. #{element} "
+  end
 end
